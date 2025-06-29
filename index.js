91a1c62614097d955c216581adc10d3ad68c7363
Decredox <122705239+Decredox@users.noreply.github.com>
2025-06-28 22:41:38 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const BonkClient = require("./bonkClient");
+
+const client = new BonkClient();
+
+client.on("ready", async ({ setAdressByUrl, setAdressByName }) => {
+  console.log("Cliente autenticado com sucesso.");
+    let room = await setAdressByUrl("https://bonk.io/573688"); 
+  console.log(room);
+});
+
+// Iniciar login
+client.login({
+  username: "RoomManager",
+  password: "mR#84vX2!qLp@Zu9Wd"
+});
